d1cbbade965c35abcc78163b61d6d803c96e913e
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2023-05-12 22:51:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('Hello World')
+PATH=os.path.dirname(os.path.realpath(__file__))+'../'
+with open(f"{PATH}config.json", "r", encoding="utf-8-sig") as f:
+	config = json.load(f)
+ print(config)
