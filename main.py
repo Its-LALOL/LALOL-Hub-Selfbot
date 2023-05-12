b0919b6b3b034e009ae63c5f7991d92b027b5da5
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2023-05-12 22:55:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-PATH=os.path.dirname(os.path.realpath(__file__))+'../'
+PATH=path.dirname(path.realpath(__file__))+'/'
 with open(f"{PATH}config.json", "r", encoding="utf-8-sig") as f:
 	config = json.load(f)
- print(config)
+print(config)
