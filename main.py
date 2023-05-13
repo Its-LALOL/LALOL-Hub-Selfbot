9d0236d25480a56a70f2ef8e28bac5400ff53644
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2023-05-13 19:54:55 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from os import path
-import json
-PATH=path.dirname(path.realpath(__file__))+'/'
-with open(f"{PATH}config.json", "r", encoding="utf-8-sig") as f:
-	config = json.load(f)
-print(config)
